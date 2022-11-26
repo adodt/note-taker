f139cb982f9990f50dac89a03eee951a6f029c97
Gator <allidodt@gmail.com>
2022-11-26 09:12:53 -0800

    fixing file tree

---
+++ routes/notes.js
@@ -0,0 +1,45 @@
+const notes = require("express").Router();
+const {
+    readFromFile,
+    readAndAppend,
+    readAndDelete,
+} = require("../helpers/uuid");
+const uuid = require("../helpers/uuid");
+
+notes.get('/', (req, res) => {
+    console.info(`${req.method} request received`);
+    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post("/", (req, res) => {
+    console.info(`${req.method} request received`);
+    const { title, text } = req.body;
+    if (title && text ) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+        readAndAppend(newNote, "./db/db.json");
+        const response = {
+            status: "success",
+            body: newNote,
+        };
+        res.json(response);
+    } else {
+        res.json("Error! Could not post note.")
+    }
+});
+
+notes.delete("/:id", (req, res) => {
+    console.info(`${req.method} Deleting note.`);
+    const id = req.params.id;
+    if (id) {
+        readAndDelete(id, "./db/db.json");
+        res.json("Note deleted.");
+    } else {
+        res.json("Error! Could not delete note.")
+    }
+});
+
+module.exports = notes;
