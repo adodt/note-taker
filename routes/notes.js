d96862ab54a18cf943a26eb6f20cd70301f9ef72
Gator <allidodt@gmail.com>
2022-11-26 10:33:56 -0800

    debugging server.js

--- routes/notes.js
+++ routes/notes.js
@@ -31,15 +31,19 @@ notes.post("/", (req, res) => {
     }
 });
 
-notes.delete("/:id", (req, res) => {
-    console.info(`${req.method} Deleting note.`);
+//DELETE note
+app.delete('api/notes/:id', (req, res) => {
     const id = req.params.id;
-    if (id) {
-        readAndDelete(id, "./db/db.json");
-        res.json("Note deleted.");
-    } else {
-        res.json("Error! Could not delete note.")
-    }
+    let note;
+
+    notes.map((element, index) => {
+        if (element.id == id) {
+            note = element
+            notes.splice(index, 1)
+            return res.json(note);
+        }
+
+    });
 });
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -9,74 +9,60 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use('/api', api);
 
-//Data from db.json
-const { notes } = require('./db/db.json')
-
-function newNote(body, notesArray) {
-    const note = body;
-    notesArray.push(note);
-
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notes: notesArray }), null, 2
-    );
-    return note;
-};
-
-//check note data
-function validateNote(note) {
-    if (!note.title || typeof note.title !== 'string') {
-        return false;
-    }
-    if (!note.text || typeof note.text !== 'string') {
-        return false;
-    }
-    return true;
-};
-
-//GET route
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
-})
 
 //server route. Accepts data to be stored on server.
 app.post('/api/notes', (req, res) => {
-    req.body.iq = notes.length.toString();
+    const { title, text } = req.body;
 
-    if (!validateNote(req.body)) {
-        res.status(400).send('Note is note formatted correctly.');
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedNotes = JSON.parse(data);
+                parsedNotes.push(newNote);
+                fs.writeFile(
+                    './db/db.json',
+                    JSON.stringify(parsedNotes, null, 4),
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Note has been posted.')
+                );
+            }
+        });
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        console.log(response);
+        res.status(201).json(response);
     } else {
-        const note = createNewNote(req.body, notes);
-        res.json(note);
+        res.status(500).json('Error. Note not posted.')
     }
+}
+);
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
-//DELETE note
-app.delete('api/notes/:id', (req, res) => {
-    const id = req.params.id;
-    let note;
-
-    notes.map((element, index) => {
-        if (element.id == id) {
-            note = element
-            notes.splice(index, 1)
-            return res.json(note);
-        }
-
-    });
-});
 
 //INDEX route
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-//NOTES.HTML route
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
+//Listen to server connection
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
