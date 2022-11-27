996ba1ba7224886adbce614701a0cdc10c0768bb
Gator <allidodt@gmail.com>
2022-11-27 08:48:28 -0800

    debugging heroku deploy

--- server.js
+++ server.js
@@ -67,19 +67,3 @@ app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    },
-  );
-}
